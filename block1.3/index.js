499dac67df9241e6e3516ec40d2627d061dae310
Alexandr Chernik <amanita1616@gmail.com>
2021-06-05 19:28:07 +0300

    commit

---
+++ block1.3/index.js
@@ -0,0 +1,13 @@
+const active = document.querySelectorAll(".services__btn");
+// active.classList.remove('active')
+active.forEach(function(item){
+  item.classList.remove('active')
+  item.addEventListener("click",function(){
+    let activeBtn = item;
+ active.forEach(function(item){
+   item.classList.remove('active')
+ })
+ activeBtn.classList.add('active')
+  })
+ 
+})
