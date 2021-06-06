068162b29442a83b47d33f62fc140f45ea91aaa8
Alexandr Chernik <amanita1616@gmail.com>
2021-06-06 22:41:21 +0300

    commit

--- block1.4/index.js
+++ block1.4/index.js
@@ -1,23 +1,30 @@
 const active = document.querySelectorAll(".nav__btn");
-active.forEach(function(item){
-  item.classList.remove('active-btn')
-  item.addEventListener("click",function(){
-    let activeBtn = item;
- active.forEach(function(item){
-   item.classList.remove('active-btn')
- })
- activeBtn.classList.add('active-btn')
+
+function navBtn(active){
+  active.forEach(function(item){
+    item.classList.remove('active-btn')
+    item.addEventListener("click",function(){
+      let activeBtn = item;
+   active.forEach(function(item){
+     item.classList.remove('active-btn')
+   })
+   activeBtn.classList.add('active-btn')
+    })
   })
-})
+}
+navBtn(active)
 
 const activeLang = document.querySelectorAll(".footer__lang-btn");
-activeLang.forEach(function(item){
-  item.classList.remove('active')
-  item.addEventListener("click",function(){
-    let activeBtnLang = item;
- activeLang.forEach(function(item){
-   item.classList.remove('active')
- })
- activeBtnLang.classList.add('active')
+function footerBtn(activeLang){
+  activeLang.forEach(function(item){
+    item.classList.remove('active')
+    item.addEventListener("click",function(){
+      let activeBtnLang = item;
+   activeLang.forEach(function(item){
+     item.classList.remove('active')
+   })
+   activeBtnLang.classList.add('active')
+    })
   })
-})
+}
+footerBtn(activeLang)
