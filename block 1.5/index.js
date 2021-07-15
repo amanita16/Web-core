abc21f5530ffc10f3191c7b65ed201da3737eb42
Alexandr Chernik <amanita1616@gmail.com>
2021-07-16 02:15:56 +0300

    commit

--- block 1.5/index.js
+++ block 1.5/index.js
@@ -1,59 +1,33 @@
-const slider = document.querySelector('.slider-container');
-const sliderItem = document.querySelector('.swiper-slide')
 
+new Swiper('.swiper-container',{
 
-
-function mobileSlider() {
-  if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
-   let mySwiper = new Swiper(slider, {
-      pagination: {
-        el: '.swiper-pagination',
-         clickable: true,
-        //  type: 'bullets',
-           },
-      slidesPerView: 1,
-      spaceBetween: 20,
-      loop: true,
-       slideClass: '.card',
-    
-           
-    });
-
-    slider.dataset.mobile = 'true';
-  }
-
-  if (window.innerWidth > 768) {
-    slider.dataset.mobile = 'false';
-     if (slider.classList.contains('swiper-container-initialized')) {
-      mySwiper.destroy();
-     }
-  }
-}
-
-mobileSlider()
-
-window.addEventListener('resize', () => {
-  mobileSlider();
-});
+          slidesPerView: 1,
+          spaceBetween: 10,
+          loop: true,
+          //  slideClass: '.swiper-slide',
+           pagination: {
+            el: '.swiper-pagination',
+             clickable: true,
+             type: 'bullets',
+               },
+})
 
 function showMore() {
-const sliderVisible = document.querySelector('.swiper-slide__visible')
-const sliderHidden = document.querySelectorAll('.hidden')
-const image = document.querySelector('.swiper-slide__visible-img')
+  const sliderVisible = document.querySelector(".brends__visible");
+  const sliderHidden = document.querySelectorAll(".hidden");
+  const image = document.querySelector(".brends__visible-img");
 
-sliderHidden.forEach((item) => {
- 
-  if (item.style.display === "none"){
-    item.style.display = "flex"
-    sliderVisible.innerHTML = "Cкрыть";
-    sliderVisible.style.marginTop = '40px'
-      image.style.transform = "rotate(180deg)"
-  }
- else{
-    item.style.display = "none";
-    sliderVisible.innerHTML = "Показать все";
-    image.style.transform = "rotate(0deg)"
-  }
-})
+  sliderHidden.forEach((item) => {
+    if (item.style.display === "none") {
+      item.style.display = "flex";
+      sliderVisible.innerHTML = "Cкрыть";
+      sliderVisible.style.marginTop = "40px";
+      image.style.transform = "rotate(180deg)";
+    } else {
+      item.style.display = "none";
+      sliderVisible.innerHTML = "Показать все";
+      image.style.transform = "rotate(0deg)";
+    }
+  });
 }
-showMore()
+showMore();
